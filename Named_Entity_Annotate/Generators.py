ff887627c1ee2e0f46d00c8d1319926dad57d2c9
Kyle W-M <25kylworc@gmail.com>
2019-09-14 14:24:55 -0700

    Converted Generators.py to use yield generators.

--- Named_Entity_Annotate/Generators.py
+++ Named_Entity_Annotate/Generators.py
@@ -5,92 +5,52 @@ from Utilities import write_to_line_number
 
 # --- Source Function Generators --
 
-def from_iterator(iterator):
-    def get_next_item():
-        try:
-            return iterator.__next__()
-        except StopIteration:
-            return None
-    return get_next_item
-
-def from_folder(folder_path):
+def from_folder(folder_path,filter_by_extension=None):
     file_list = []
     for filename in os.listdir(folder_path):
-        if filename.endswith(".json"):
-            file_list.append(os.path.join(folder_path,filename))
-    file_iterator = iter(file_list)
-
-    def get_next_item():
-        try:
-            file_path = file_iterator.__next__()
-            with open(file_path,'r') as file:
-                return file.read()
-        except StopIteration:
-            return None
-    return get_next_item
-
-def from_file(file_path):
-    file = open(file_path,'r')
-    def get_next_item():
-        line = None
-        if not file.closed:
-            line = file.readline()
-        if line == None or line.strip() == '':
-            file.close()
-            return None
-        else:
-            return line
-    return get_next_item
+        if filter_by_extension == None or filename.endswith(filter_by_extension):
+            with open(os.path.join(folder_path,filename),'r') as file:
+                yield file.read()
 
 # --- Modifier Function Generators --
 
-def make_empty_ent_dict_from_text(string_source_fn):
-    def get_item():
-        string = string_source_fn()
+def make_empty_ent_dict_from_text(string_source_gen):
+    for string in string_source_gen:
         if string == None:
-            return None
+            yield None
         else:
-            return {
+            yield {
                 "text":str(string),
                 "ents":[]
             }
-    return get_item
 
-def parse_json_string(string_source_fn):
-    def get_item():
-        string = string_source_fn()
+def parse_json_string(string_source_gen):
+    for string in string_source_gen:
         if string == None:
-            return None
+            yield None
         else:
-            return json.loads(string)
-    return get_item
+            yield json.loads(string)
 
-def dump_json_string(dict_source_fn):
-    def get_item():
-        dict_source = dict_source_fn()
+def dump_json_string(dict_source_gen):
+    for dict_source in dict_source_gen:
         if dict_source == None:
-            return None
+            yield None
         else:
-            return json.dumps(dict_source)
-    return get_item
+            yield json.dumps(dict_source)
 
-def escape_control_chars(string_source_fn):
-    def get_item():
-        string = string_source_fn()
+def escape_control_chars(string_source_gen):
+    for string in string_source_gen:
         if string == None:
-            return None
+            yield None
         else:
-            return string.encode('unicode_escape')
-    return get_item
+            yield string.encode('unicode_escape')
 
-def unescape_control_chars(string_source_fn):
-    def get_item():
-        string = string_source_fn()
+def unescape_control_chars(string_source_gen):
+    for string in string_source_gen:
         if string == None:
-            return None
+            yield None
         else:
-            return string.decode('unicode_escape')
-    return get_item
+            yield string.decode('unicode_escape')
 
 # --- Save Function Generators --
 
@@ -101,21 +61,32 @@ def save_line_to_file(file_path):
             file.close()
             return None
         else:
-            return write_to_line_number(file,string,item_index)
+            return Utilities.write_to_line_number(file,string,item_index)
+    return save_item
+
+def save_as_file_in_folder(folder_path,output_file_extension):
+    def save_item(content_string,item_id):
+        file_name = item_id + '.' + output_file_extension
+        with open(os.path.join(folder_path,file_name),'w') as file:
+            file.write(content_string)
+        return True
     return save_item
 
 
 
-next = make_empty_ent_dict_from_text(from_iterator(iter([1,2,4])))
-# next = make_empty_ent_dict_from_text(unescape_control_chars(escape_control_chars(from_folder('./'))))
+# nexty = make_empty_ent_dict_from_text(iter([1,2,4]))
+nexty = make_empty_ent_dict_from_text(unescape_control_chars(escape_control_chars(from_folder('./',filter_by_extension='rc'))))
 # next = from_file('README.md')
 
-print(parse_json_string(dump_json_string(next))())
-print(dump_json_string(next)())
-print(parse_json_string(dump_json_string(next))())
-print(parse_json_string(dump_json_string(next))())
-print(next())
-print(next())
+# print(next(nexty))
+# print(next(nexty))
+# print(next(nexty))
+# print(next(nexty))
+# print(next(nexty))
+print(next(dump_json_string(nexty)))
+print(next(parse_json_string(dump_json_string(nexty))))
+print(next(parse_json_string(dump_json_string(nexty))))
+print(next(parse_json_string(dump_json_string(nexty))))
 # print(next())
 # print(next())
 # print(next())
--- Named_Entity_Annotate/Utilities.py
+++ Named_Entity_Annotate/Utilities.py
@@ -10,3 +10,18 @@ def write_to_line_number(file_ref,line_string,line_index):
                 temp_file.write(line_string)
             line = file_ref.readline()
             line_num += 1
+
+def merge_entities_without_overlaps(existing_entity_list,lower_priority_entity_list):
+    ents = existing_entity_list.copy()
+    print('source_ents:')
+    print(ents)
+    print('lower_priority_ents:')
+    print(lower_priority_entity_list)
+    for low_priority_ent in lower_priority_entity_list:
+        for i in range(len(existing_entity_list)):
+            previous_existing_ent = existing_entity_list[i - 1] or None
+            current_existing_ent = existing_entity_list[i] or None
+            # Here we check if the low_priority_ent we want is between the end of the previous_exising_ent and start of the next existing entity (current_existing_ent):
+            if previous_existing_ent.get("end",-1) < low_priority_ent.get('start',0) and low_priority_ent.get('end',0) < current_existing_ent.get('start',1000000000000):
+                ents.insert(i - 1, low_priority_ent)
+    return ents
